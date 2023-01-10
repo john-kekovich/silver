c52eb520e492cf16daf7c3ad7c819dcba0505818
DESKTOP-ELR71P0\gabu <gksakic@gmail.com>
2023-01-11 00:54:30 +0900

    Adding new page endpoint

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/tinko', methods=['GET'])
+def tinko():
+    return render_template('tinko.html')
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
